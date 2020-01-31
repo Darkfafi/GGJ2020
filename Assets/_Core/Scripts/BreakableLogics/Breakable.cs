b5a103c8957848f265f506f563e558bcd0bf7d52
Ramses Di Perna <ik_ben_ramses@hotmail.com>
2020-01-31 22:49:51 +0100

    Created Outline, breakable component and BreakableEntity

---
+++ Assets/_Core/Scripts/BreakableLogics/Breakable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+	public event Action<State> StateChangedEvent;
+
+	public enum State
+	{
+		Unbroken,
+		Broken
+	}
+
+	public State BreakState
+	{
+		get; private set;
+	}
+
+	public void Break()
+	{
+		// TODO: Set Art to Broken
+		BreakState = State.Broken;
+		FireStateChangedEvent();
+	}
+
+	public void Repair()
+	{
+		// TODO: Set Art to Repaired
+		BreakState = State.Unbroken;
+		FireStateChangedEvent();
+	}
+
+
+	private void FireStateChangedEvent()
+	{
+		if(StateChangedEvent != null)
+		{
+			StateChangedEvent(BreakState);
+		}
+	}
+}
---
+++ Assets/_Core/Scripts/OutlineAnimator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Outline3D))]
+public class OutlineAnimator : MonoBehaviour
+{
+	[SerializeField]
+	private float _minSizeOutline = 2f;
+
+	[SerializeField]
+	private float _maxSizeOutline = 4f;
+
+	private Outline3D _outline3D;
+	private Coroutine _animationRoutine;
+
+	public bool IsAnimating
+	{
+		get
+		{
+			return _animationRoutine != null;
+		}
+	}
+
+    protected void Awake()
+	{
+		_outline3D = gameObject.GetComponent<Outline3D>();
+		StopOutlineAnimation();
+	}
+
+	public void StartOutlineAnimation()
+	{
+		StopOutlineAnimation();
+		_animationRoutine = StartCoroutine(OutlineAnimationRoutine());
+	}
+
+	public void StopOutlineAnimation()
+	{
+		if (_animationRoutine != null)
+		{
+			StopCoroutine(_animationRoutine);
+		}
+		_outline3D.OutlineWidth = 0f;
+	}
+
+	private IEnumerator OutlineAnimationRoutine()
+	{
+		while(true)
+		{
+			_outline3D.OutlineWidth = _minSizeOutline + Mathf.Abs(Mathf.Sin(Time.time)) * _maxSizeOutline;
+			yield return null;
+		}
+	}
+}
