1d1f7fc65a328b158e55367e30a20aa1293dfda9
happy30 <jordivdhappy@gmail.com>
2020-02-01 10:18:43 +0100

    Follow Entity script for during puzzles

---
+++ Assets/_Core/Scripts/Camera/FollowEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EventObjects;
+using UnityEngine;
+
+public class FollowEntity : MonoBehaviour
+{
+    public TransformWithEvent EntityLocation;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            EntityLocation.SetValue(transform);
+        }
+    }
+}
