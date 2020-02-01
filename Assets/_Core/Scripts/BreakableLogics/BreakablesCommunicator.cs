1e7206a18d44c5d8bf08e2d86ea7d62f2cab9811
Ramses Di Perna <ik_ben_ramses@hotmail.com>
2020-02-01 13:25:22 +0100

    BreakablesCommunicator get and filters added

--- Assets/_Core/Scripts/BreakableLogics/BreakablesCommunicator.cs
+++ Assets/_Core/Scripts/BreakableLogics/BreakablesCommunicator.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 
 public class BreakablesCommunicator
 {
@@ -24,6 +25,35 @@ public class BreakablesCommunicator
 	public Breakable[] GetBreakables()
 	{
 		return _registeredBreakables.ToArray();
+	}
+
+	public Breakable[] GetBreakables(System.Predicate<Breakable> filter)
+	{
+		List<Breakable> breakablesToReturn = new List<Breakable>();
+		for(int i = 0; i < _registeredBreakables.Count; i++)
+		{
+			if(filter(_registeredBreakables[i]))
+			{
+				breakablesToReturn.Add(_registeredBreakables[i]);
+			}
+		}
+		return breakablesToReturn.ToArray();
+	}
+
+	public Breakable GetClosestBrokenBreakableToLocation(Vector3 location)
+	{
+		float dist = 0f;
+		Breakable breakable = null;
+		for (int i = 0; i < _registeredBreakables.Count; i++)
+		{
+			float breakableDist = Vector3.Distance(location, _registeredBreakables[i].transform.position);
+			if (breakable == null || breakableDist < dist)
+			{
+				breakable = _registeredBreakables[i];
+				dist = breakableDist;
+			}
+		}
+		return breakable;
 	}
 
 	public void RegisterBreakable(Breakable breakable)
