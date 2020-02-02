d1a6cfc7011bb9f551a23900cb4697bfb36e43c5
Ramses Di Perna <ik_ben_ramses@hotmail.com>
2020-02-02 12:23:59 +0100

    Fixed Ghost movement and made it look better + wait at start for x seconds

--- Assets/_Core/Scripts/Ghost/GhostMovement.cs
+++ Assets/_Core/Scripts/Ghost/GhostMovement.cs
@@ -7,6 +7,13 @@ public class GhostMovement : MonoBehaviour
 {
     public GameObject theObject;
     public Renderer[] myRenderer;
+
+	[SerializeField]
+	private Behaviour _haloComp;
+
+	[SerializeField]
+	private Vector3 _offset = Vector3.zero;
+
     public void MoveTowardsObject(GameObject obj)
     {
         Vector3 diff = obj.transform.position - transform.position;
@@ -16,11 +23,15 @@ public class GhostMovement : MonoBehaviour
             r.material.DOFade(0f, 0.5f).SetEase(Ease.InOutBounce);
         }
 
-        transform.DOLookAt(obj.transform.position, 0.5f, AxisConstraint.Y, Vector3.up);
+		_haloComp.enabled = false;
+
+		transform.DOLookAt(obj.transform.position, 0.5f, AxisConstraint.Y, Vector3.up);
         transform.DOMove(transform.position + diff.normalized * 2f, 1f).OnComplete(() =>
-        {
-            transform.position = obj.transform.position + (transform.position - obj.transform.position).normalized * 4f;
-            transform.DOMove(obj.transform.position + diff.normalized * -2.5f, 1f);
+		{
+			_haloComp.enabled = true;
+			Vector3 targetMovementPos = obj.transform.position + _offset;
+            transform.position = targetMovementPos + (transform.position - targetMovementPos).normalized * 4f;
+            transform.DOMove(targetMovementPos + diff.normalized * -2.5f, 1f);
             foreach (Renderer r in myRenderer)
             {
                 r.material.DOFade(0.75f, 0.5f).SetEase(Ease.InOutBounce);
