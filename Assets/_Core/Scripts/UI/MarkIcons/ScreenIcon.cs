6f09d31d26a951dec9b2a09ac5aaf57bb59c46b7
Darkfafi <ik_ben_ramses@hotmail.com>
2020-02-08 20:33:36 +0100

    Corrected some icon placement calculations so it is relative to the camera rotation and position.

--- Assets/_Core/Scripts/UI/MarkIcons/ScreenIcon.cs
+++ Assets/_Core/Scripts/UI/MarkIcons/ScreenIcon.cs
@@ -12,18 +12,11 @@ public class ScreenIcon : CanvasItem
 	[SerializeField]
 	private Vector2 _offsetInScreenIndication = Vector2.zero;
 
-	private Transform _relativeOrigin;
 	private Transform _currentTarget;
-
-	protected override void Awake()
-	{
-		base.Awake();
-		_relativeOrigin = FindObjectOfType<PlayerMovement>().transform;
-	}
 
 	protected void Update()
 	{
-		if (_currentTarget != null)
+		if (Camera != null && _currentTarget != null)
 		{
 			if(IsInViewport(_currentTarget.transform.position, out Vector3 viewportPos))
 			{
@@ -41,18 +34,34 @@ public class ScreenIcon : CanvasItem
 			}
 
 			_iconContainer.SetActive(true);
-			Vector3 relativePosDir = (_relativeOrigin.transform.position - _currentTarget.transform.position).normalized;
-			bool xLock = Mathf.Abs(relativePosDir.x) >= Mathf.Abs(relativePosDir.z);
-			relativePosDir.x = xLock ? relativePosDir.x / Mathf.Abs(relativePosDir.x) : relativePosDir.x;
-			relativePosDir.z = !xLock ? relativePosDir.z / Mathf.Abs(relativePosDir.z) : relativePosDir.z;
-			relativePosDir.x = Mathf.Clamp(relativePosDir.x, -0.92f, 0.92f);
-			relativePosDir.z = Mathf.Clamp(relativePosDir.z, -0.88f, 0.88f);
-			Vector2 screenPos = new Vector2(CanvasSize.x * relativePosDir.x, CanvasSize.y * relativePosDir.z);
+			Vector3 relativePosDir3D = (Camera.transform.position - _currentTarget.transform.position).normalized;
+			Vector2 relativePosDir2D = new Vector2(relativePosDir3D.x, relativePosDir3D.z);
+
+			Vector3 forward = Camera.transform.forward;
+			forward = Vector3.Normalize(forward);
+			Vector3 right = Quaternion.Euler(new Vector3(0f, 90f, 0f)) * forward;
+			Vector3 relativeCameraRotation = (right + forward).normalized;
+
+			float angle = 45f + (Mathf.Atan2(relativeCameraRotation.z, -relativeCameraRotation.x) + Mathf.Atan2(relativePosDir2D.y, relativePosDir2D.x)) * Mathf.Rad2Deg;
+
+			relativePosDir2D.x = Mathf.Cos(angle * Mathf.Deg2Rad);
+			relativePosDir2D.y = Mathf.Sin(angle * Mathf.Deg2Rad);
+
+			bool xLock = Mathf.Abs(relativePosDir2D.x) >= Mathf.Abs(relativePosDir2D.y);
+			relativePosDir2D.x = xLock ? relativePosDir2D.x / Mathf.Abs(relativePosDir2D.x) : relativePosDir2D.x;
+			relativePosDir2D.y = !xLock ? relativePosDir2D.y / Mathf.Abs(relativePosDir2D.y) : relativePosDir2D.y;
+			relativePosDir2D.x = Mathf.Clamp(relativePosDir2D.x, -0.92f, 0.92f);
+			relativePosDir2D.y = Mathf.Clamp(relativePosDir2D.y, -0.88f, 0.88f);
+			Vector2 screenPos = new Vector2(CanvasSize.x * relativePosDir2D.x, CanvasSize.y * relativePosDir2D.y);
 			screenPos.x = screenPos.x.Map(-CanvasSize.x, CanvasSize.x, -CanvasSize.x / 2, CanvasSize.x / 2);
 			screenPos.y = screenPos.y.Map(-CanvasSize.y, CanvasSize.y, -CanvasSize.y / 2, CanvasSize.y / 2);
 			screenPos.x = Mathf.Lerp(RectTransform.anchoredPosition.x, screenPos.x, Time.deltaTime * 5f);
 			screenPos.y = Mathf.Lerp(RectTransform.anchoredPosition.y, screenPos.y, Time.deltaTime * 5f);
 			RectTransform.anchoredPosition = screenPos;
+		}
+		else
+		{
+			_iconContainer.SetActive(false);
 		}
 	}
 
